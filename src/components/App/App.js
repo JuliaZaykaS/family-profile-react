4002cf3eb377be5cd9b05cffa306c7155ba17bc1
JuliaZaykaS <sys55@mail.ru>
2021-10-26 13:06:55 +0300

    test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { ProfileView } from '../../views/ProfileView/ProfileView';
-import { FormsView } from '../../views/FormsView/FormsView';
+// import { ProfileView } from '../../views/ProfileView/ProfileView';
+// import { FormsView } from '../../views/FormsView/FormsView';
 import { Navigation } from '../Navigation/Navigation';
+import { TestForm } from '../testForm/TestForm';
 
 export const App = () => {
   return (
       <div>
       <Navigation />
-      <FormsView/>
-      <ProfileView />
+      {/* <FormsView/> */}
+      {/* <ProfileView /> */}
+      <TestForm/>
     </div>
   );
 };
